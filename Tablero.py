e3eda214b07402942a5f8c01cd224f4553d7aee2
Erick <efmz2000@outlook.es>
2022-02-18 18:59:06 -0600

    codigo tablero

--- Tablero.py
+++ Tablero.py
@@ -1,20 +1,30 @@
+from traceback import print_tb
 from Ficha import *
 
 class Tablero:
     
     #Defina aquí los atributos de Tablero
-    jugadores=[]
+    jugadores=[]    
+    
 
 
-    #también va a necesitar una lista de Fichas (puede asumir un número de Fichas fijo si le parece más fácil), 
-    #y un mecanismo para saber quién sigue en el turno
-
-
-
+   
 
     #agregue parámetros necesarios después de self
     #para iniciar los valores de sus atributos
-    def __init__(self):
+    def __init__(self,cantidad_casiilas):
+        self.cantidad_casillas=cantidad_casiilas
+        self.jugador_actual=0
+    
+    def agregar_jugador(self):
+        color=input("Ingrese el color de la nueva ficha")
+        self.jugadores+=[Ficha(),color]
+    
+    def mostrar_orden(self):
+        print("La meta esta en la posición: "+str(self.cantidad_casillas))
+        for player in self.jugadores:
+            print("El jugador: "+player.color+"esta en la casilla: "+str(player.poscion))
+        
         
         #inicialice aquí todos los atributos
         #no olvide usar self.atributo para acceder el atributo de la clase
